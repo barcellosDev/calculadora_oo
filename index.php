c1e5a0908decde5f3fee3bea6eb3b6fb779be8d5
barcellosDev <zzzsoldierbrzzz@gmail.com>
2018-11-07 18:31:32 -0200

    new calc

--- index.php
+++ index.php
@@ -1,42 +1,53 @@
 <!doctype html>
-<html>
-<head>
-<title>Calculadora</title>
-<meta charset="utf-8">
-<style type="text/css">
-  #principal {
-    border-style: ridge;
-    border-radius: 3px;
-    border-width: 2px;
-    border-color: black;
-  }
-</style>
-</head>
-<body>
-<div align="center" id="principal">
-  <form method="get">
-    <input type="number" name="num1"><br>
-    <select name="operacao">
-      <option selected value="selecione">Selecione</option>
-      <option value="+">+</option>
-      <option value="-">-</option>
-      <option value="*">*</option>
-      <option value="/">/</option>
-      <option value="%">%</option>
-      <option value="**">**</option>
-    </select>
-    <br>
-    <input type="number" name="num2"><br>
-    <input type="submit" name="calcular" value="Calcular">
-    <input type="submit" name="limpa" value="Limpar">
-  </form>
-  <br>
-  <br>
-  <div>
-    <p><strong>Resultado</strong></p>
-    <br>
-    <?php include ("calc.php"); ?>
-  </div>
-</div>
-</body>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <style media="screen">
+      .align-center
+      {
+        text-align: center;
+      }
+    </style>
+    <title>Calculadora</title>
+  </head>
+  <body>
+    <br><br><br><br><br><br><br><br><br>
+    <form method="post">
+      <input class="form-control form-control-lg" type="text" name="conta" placeholder="Insira sua expressão aqui"><br>
+      <div class="align-center">
+        <button type="submit" name="calcula" class="btn btn-primary btn-lg">Calcular</button>
+        <button type="submit" name="apaga" class="btn btn-secondary btn-lg">Clear</button>
+      </div>
+    </form>
+  </body>
 </html>
+<?php
+/*
+$x = 2;
+$y = 3;
+$z = eval('return $x + $y;');
+eval('echo $z;');
+*/
+
+if (isset($_POST['apaga']))
+{
+  header("Location: index.php");
+}
+if (isset($_POST['calcula']))
+{
+  $expressao = (!empty($_POST['conta']) and !preg_match("/[a-zA-Z]/", $_POST['conta'])) ? (string)$_POST['conta'] : false;
+  if (!$expressao)
+  {
+    echo '<h2>Erro!</h2>';
+  } else
+  {
+    $result = eval("return $expressao;");
+    echo "<h2>Resultado: ".$result."</h2>";
+  }
+}
+?>
